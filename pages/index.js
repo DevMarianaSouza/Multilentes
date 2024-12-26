e954c0ae17866605f7c551c49f00b72410cc9daf
Mariana Souza <148583625+DevMarianaSouza@users.noreply.github.com>
2024-12-26 11:36:13 -0300

    Fix

--- pages/index.js
+++ pages/index.js
@@ -118,7 +118,7 @@ export default function Home() {
                     </div>
                 
                 </div>
-                <div styles={{ 
+                <div style={{ 
                         height: '100%',
                         position: 'relative',
                         display: 'flex',
